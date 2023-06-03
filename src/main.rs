e958f03064ce419c14f6216559ad563341127add
Evan Simkowitz <esimkowitz@wustl.edu>
2023-06-03 16:37:31 -0400

    Attempting to add sidebar buttons but click events aren't propagating

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,38 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
+    let current_widget = use_state(cx, || CurrentWidget::NumberBaseConverter);
+
+    fn set_display(current_widget: &UseState<CurrentWidget>, desired_widget: CurrentWidget) -> &str {
+        if *current_widget.get() == desired_widget { "block" } else { "none" }
+    }
+
     cx.render(rsx! {
-        number_base_converter::NumberBaseConverter {}
-        base64_converter::Base64Converter {}
+        div {
+            div {
+                button {
+                    onclick: move |_| current_widget.set(CurrentWidget::NumberBaseConverter),
+                    "Number base converter"
+                }
+                button {
+                    onclick: move |_| current_widget.set(CurrentWidget::Base64Converter),
+                    "Base64 converter"
+                }
+            }
+            div {
+                display: set_display(current_widget, CurrentWidget::Base64Converter),
+                base64_converter::Base64Converter {}
+            }
+            div {
+                display: set_display(current_widget, CurrentWidget::NumberBaseConverter),
+                number_base_converter::NumberBaseConverter {}
+            }
+        }
     })
+}
+
+#[derive(PartialEq)]
+enum CurrentWidget {
+    NumberBaseConverter,
+    Base64Converter,
 }
