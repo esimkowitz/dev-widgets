1f6c3fa7767c4e7cbf8e631a6b094ae00cbe28d2
Evan Simkowitz <esimkowitz@wustl.edu>
2023-06-10 14:53:49 -0700

    Fix bounce on whole window

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() {
                 r#"
                 <link rel="stylesheet" href="../style/bootstrap.min.css">
                 <link rel="stylesheet" href="../style/style.css">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, height=device-height">
                 <title>Dev Widgets</title>
                 "#
                 .to_string(),
