a7201d65cde29c5097489c730c0dacd2326130bc
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2023-06-16 22:31:46 -0700

    Migrate stylesheets to Sass (#28)
    This converts the main style.css to a Sass stylesheet. It also moves the
    Bootstrap install code to a pure Rust script. The Sass stylesheets are
    compiled into CSS using the grass crate.

--- scripts/install_bootstrapcss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Install Bootstrap CSS
-echo "Installing Bootstrap CSS..."
-cd $CARGO_MANIFEST_DIR/style
-curl -o "bootstrap.min.css" https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css
-cd -
-echo "Bootstrap CSS installed"
