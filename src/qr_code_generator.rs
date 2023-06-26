df3a3093eb78b59497f4e7427bd6f32baae22b76
Evan Simkowitz <esimkowitz@wustl.edu>
2023-06-26 12:12:43 -0700

    blah

--- src/qr_code_generator.rs
+++ src/qr_code_generator.rs
@@ -1,7 +1,8 @@
-use base64::{engine::general_purpose, Engine as _};
+use std::io::{Error, ErrorKind};
+
 use dioxus::prelude::*;
 use dioxus_free_icons::icons::bs_icons::BsQrCode;
-use qrcode_generator::QrCodeEcc;
+use qrcodegen::{QrCode, QrCodeEcc};
 
 use crate::{
     select_form::SelectForm,
@@ -23,13 +24,13 @@ const ICON: WidgetIcon<BsQrCode> = WidgetIcon { icon: BsQrCode };
 pub fn qr_code_generator(cx: Scope) -> Element {
     let qr_code_value = use_state(cx, || "".to_string());
     let qr_code_error_correction = use_state(cx, || QrCodeEcc::Low);
-
-    let result = qrcode_generator::to_png_to_vec_from_str(
-        qr_code_value.get(),
+    let qrcode = QrCode::encode_text(
+        qr_code_value.get().as_str(),
         *qr_code_error_correction.get(),
-        1024,
     )
     .unwrap();
+    const QRCODE_SIZE: i64 = 300;
+    let path_string = generate_qrcode_path_str(qrcode, QRCODE_SIZE as usize).unwrap();
 
     cx.render(rsx! {
         div {
@@ -54,10 +55,53 @@ pub fn qr_code_generator(cx: Scope) -> Element {
                     qr_code_value.set(event.value.clone());
                 }
             }
-            img {
+            svg {
                 class: "qr-code",
-                src: format_args!("data:image/png;base64,{}", general_purpose::STANDARD.encode(result)),
+                stroke: "#000",
+                height: QRCODE_SIZE,
+                width: QRCODE_SIZE,
+                view_box: "0 0 {QRCODE_SIZE} {QRCODE_SIZE}",
+                xmlns: "http://www.w3.org/2000/svg",
+                fill: "#000",
+                title {
+                    "Generated QR Code"
+                }
+                path {
+                    d: "{path_string}"
+                }
             }
         }
     })
+}
+
+fn generate_qrcode_path_str(qrcode: QrCode, size: usize) -> Result<String, Error> {
+    let mut path_string = String::new();
+    let margin_size = 1;
+
+    let s = qrcode.size();
+
+    let data_length = s as usize;
+
+    let data_length_with_margin = data_length + 2 * margin_size;
+
+    let point_size = size / data_length_with_margin;
+
+    if point_size == 0 {
+        return Err(Error::new(ErrorKind::Other, "The size is too small"));
+    }
+
+    let margin = (size - (point_size * data_length)) / 2;
+
+    for i in 0..s {
+        for j in 0..s {
+            if qrcode.get_module(j, i) {
+                let x = j as usize * point_size + margin;
+                let y = i as usize * point_size + margin;
+
+                path_string
+                    .push_str(format!("M{x} {y}h{point_size}v{point_size}H{x}V{y}").as_str());
+            }
+        }
+    }
+    return Ok(path_string);
 }
