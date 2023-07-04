06824ff163fdd8bdd01581aa44f69585ff2b9fa8
Evan Simkowitz <esimkowitz@wustl.edu>
2023-07-04 02:29:41 -0700

    Add crude dropdown for selecting timezone

--- src/pages/date_converter.rs
+++ src/pages/date_converter.rs
@@ -1,7 +1,11 @@
+use std::{fmt::Display, str::FromStr};
+
 use dioxus::prelude::*;
 use dioxus_free_icons::icons::bs_icons::BsClock;
+use strum::IntoEnumIterator;
+use chrono_tz::{TZ_VARIANTS, Tz, ParseError};
 
-use crate::widget_entry::{WidgetEntry, WidgetIcon};
+use crate::{widget_entry::{WidgetEntry, WidgetIcon}, components::inputs::{SelectFormEnum, SelectForm}};
 
 pub const WIDGET_ENTRY: WidgetEntry = WidgetEntry {
     title: "Date Converter",
@@ -17,8 +21,59 @@ const ICON: WidgetIcon<BsClock> = WidgetIcon { icon: BsClock };
 pub fn date_converter(cx: Scope) -> Element {
     cx.render(rsx! {
         div {
-            class: "date-converter"
-            
+            class: "date-converter",
+            SelectForm::<TimeZone> {
+                label: "Time Zone",
+                oninput: move |tz: TimeZone| {
+                    println!("Time Zone: {}", tz);
+                }
+            }
         }
     })
 }
+
+#[derive(Copy, Clone, Debug, Hash)]
+enum TimeZone {
+    Base(Tz),
+}
+
+impl Default for TimeZone {
+    fn default() -> Self {
+        Self::Base(Tz::UTC)
+    }
+}
+
+impl Display for TimeZone {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Base(tz) => write!(f, "{}", tz),
+        }
+    }
+}
+
+impl FromStr for TimeZone {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::Base(Tz::from_str(s)?))
+    }
+
+    type Err = ParseError;
+}
+
+impl IntoEnumIterator for TimeZone {
+    fn iter() -> Self::Iterator {
+        TZ_VARIANTS.iter().map(|tz| Self::Base(*tz)).collect::<Vec<_>>().into_iter()
+    }
+
+    type Iterator = std::vec::IntoIter<Self>;
+}
+
+impl From<TimeZone> for &'static str {
+    fn from(val: TimeZone) -> Self {
+        match val {
+            TimeZone::Base(tz) => tz.name(),
+        }
+    }
+}
+
+
+impl SelectFormEnum for TimeZone {}
