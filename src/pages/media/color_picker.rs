5d8154efb882807b01f667921c3bcb00868044f3
Evan Simkowitz <esimkowitz@wustl.edu>
2024-04-21 15:46:26 -0700

    merge color picker pr

--- src/pages/media/color_picker.rs
+++ src/pages/media/color_picker.rs
@@ -1,5 +1,12 @@
 #![allow(non_snake_case)]
-use dioxus::{html::geometry::{euclid::{Rect, Point2D}, PageSpace}, prelude::*};
+use color_processing::Color;
+use dioxus::{
+    html::geometry::{
+        euclid::{default, Point2D, Rect},
+        PageSpace,
+    },
+    prelude::{SvgAttributes, *},
+};
 use dioxus_free_icons::icons::bs_icons::BsEyedropper;
 
 use crate::pages::{WidgetEntry, WidgetIcon};
@@ -14,63 +21,75 @@ pub const WIDGET_ENTRY: WidgetEntry = WidgetEntry {
 const ICON: WidgetIcon<BsEyedropper> = WidgetIcon { icon: BsEyedropper };
 
 pub fn ColorPicker() -> Element {
-    use_context_provider(|| Color {
-        hue: 0f64,
-        saturation: 50f64,
-        lightness: 50f64,
+    use_context_provider(|| {
+        Signal::new(ColorPickerState {
+            hue: 0.0,
+            saturation: 1.0,
+            brightness: 1.0,
+            alpha: 1.0,
+        })
     });
 
     rsx! {
         div {
             class: "color-picker",
             ColorWheel {}
+            SaturationBrightnessBox {}
             ColorView {}
         }
     }
 }
 
 fn ColorWheel() -> Element {
-    let mut color_state = use_context::<Color>();
-    let dimensions = use_signal(Rect::<f64, PageSpace>::zero);
+    let mut color_state = use_context::<Signal<ColorPickerState>>();
     let mut tracking_state = use_signal(|| false);
+    let dimensions = use_signal(Rect::<f64, f64>::zero);
 
     let mut process_mouse_event = move |event: Event<MouseData>| {
         let cursor_coordinates = event.data.page_coordinates();
         let center_coordinates = dimensions.with(|rect| rect.center().cast_unit());
-        color_state.hue = cursor_position_to_hue(cursor_coordinates, center_coordinates);
+        color_state.write().hue = cursor_position_to_hue(cursor_coordinates, center_coordinates);
     };
-    
+
     rsx! {
         div {
             class: "colorwheel-wrapper",
-            onmounted: move |_| {
-                to_owned![dimensions];
-                async move {
-                    // if let Ok(rect) = get_client_rect().await {
-                    //     dimensions.set(rect);
-                    // }
-                }
-            },
-            onmousedown: move |_| {
-                tracking_state.set(true);
-            },
-            onmouseup: move |_| {
-                tracking_state.set(false);
-            },
-            onmouseleave: move |_| {
-                tracking_state.set(false);
-            },
-            onmousemove: move |event| {
-                if *tracking_state.read() {
+            div {
+                class: "colorwheel",
+                onmounted: move |cx| {
+                    to_owned![dimensions];
+                    async move {
+                        if let Ok(rect) = cx.get_client_rect().await {
+                            dimensions.set(rect);
+                        }
+                    }
+                },
+                onmousedown: move |event| {
+                    event.stop_propagation();
+                    tracking_state.set(true);
+                },
+                onmouseup: move |event| {
+                    event.stop_propagation();
+                    tracking_state.set(false);
+                },
+                onmouseleave: move |event| {
+                    event.stop_propagation();
+                    tracking_state.set(false);
+                },
+                onmousemove: move |event| {
+                    event.stop_propagation();
+                    if *tracking_state.read() {
+                        process_mouse_event(event);
+                    }
+                },
+                onclick: move |event| {
+                    event.stop_propagation();
                     process_mouse_event(event);
+                },
+                ColorWheelSvg {}
+                ColorWheelCursorSvg {
+                    hue: color_state.read().hue,
                 }
-            },
-            onclick: move |event| {
-                process_mouse_event(event);
-            },
-            ColorWheelSvg {}
-            ColorWheelCursorSvg {
-                hue: color_state.hue,
             }
         }
     }
@@ -113,12 +132,100 @@ fn ColorWheelSvg() -> Element {
 #[component]
 fn ColorWheelCursorSvg(hue: f64) -> Element {
     rsx! {
+        CursorPrimitiveSvg {
+            class: "colorwheel-cursor",
+            fill: "hsl({hue}deg, 100%, 50%)",
+            transform: "rotate({hue_to_css_rotation(hue)} 50 50)",
+        }
+    }
+}
+
+fn SaturationBrightnessBox() -> Element {
+    let mut color_state = use_context::<Signal<ColorPickerState>>();
+    let mut tracking_state = use_signal(|| false);
+    let dimensions = use_signal(Rect::<f64, f64>::zero);
+
+    let mut process_mouse_event = move |event: Event<MouseData>| {
+        let cursor_coordinates = event.data.element_coordinates();
+        log::trace!("cursor_coordinates: {:?}", cursor_coordinates);
+        log::trace!(
+            "dimensions_min: {:?}, dimensions_max: {:?}, dimensions_size: {:?}",
+            dimensions.read().min(),
+            dimensions.read().max(),
+            dimensions.read().size
+        );
+        let sv_scale = default::Scale::new(dimensions.read().size.width / 100.0);
+        let point_sv = cursor_coordinates.cast_unit() / sv_scale;
+        log::trace!("point_sv: {:?}", point_sv);
+        color_state.write().saturation = x_axis_to_saturation(point_sv.x);
+        color_state.write().brightness = y_axis_to_brightness(point_sv.y);
+    };
+
+    rsx! {
+        div {
+            class: "saturation-brightness-wrapper",
+            div {
+                class: "saturation-brightness-box",
+                onmounted: move |cx| {
+                    to_owned![dimensions];
+                    async move {
+                        if let Ok(rect) = cx.get_client_rect().await {
+                            dimensions.set(rect);
+                        }
+                    }
+                },
+                onmousedown: move |event| {
+                    event.stop_propagation();
+                    tracking_state.set(true);
+                },
+                onmouseup: move |event| {
+                    event.stop_propagation();
+                    tracking_state.set(false);
+                },
+                onmouseleave: move |event| {
+                    event.stop_propagation();
+                    tracking_state.set(false);
+                },
+                onmousemove: move |event| {
+                    event.stop_propagation();
+                    if *tracking_state.read() {
+                        process_mouse_event(event);
+                    }
+                },
+                onclick: move |event| {
+                    event.stop_propagation();
+                    process_mouse_event(event);
+                },
+                div {
+                    class: "saturation-brightness-gradient",
+                    style: "background-color: hsl({color_state.read().hue}deg, 100%, 50%);"
+                }
+                CursorPrimitiveSvg {
+                    class: "saturation-brightness-cursor",
+                    fill: "{color_state.read().get_rgb_string()}",
+                    x: saturation_to_x_axis(color_state.read().saturation),
+                    y: brightness_to_y_axis(color_state.read().brightness),
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn CursorPrimitiveSvg(
+    x: Option<f64>,
+    y: Option<f64>,
+    class: Option<String>,
+    fill: String,
+    transform: Option<String>,
+) -> Element {
+    rsx! {
         svg {
             view_box: "0 0 100 100",
-            class: "colorwheel-cursor",
+            class: class.unwrap_or("\\".to_string()),
             defs {
                 radialGradient {
-                    id: "colorwheel-cursor-border",
+                    id: "cursor-border",
                     stop {
                         offset: "0%",
                         stop_color: "white",
@@ -147,14 +254,14 @@ fn ColorWheelCursorSvg(hue: f64) -> Element {
                 }
             }
             g {
-                transform: "rotate({hue_to_css_rotation(hue)} 50 50)",
+                transform: transform.unwrap_or("\\".to_string()),
                 circle {
-                    cx: 50,
-                    cy: 3.75,
+                    cx: x.unwrap_or(50f64),
+                    cy: y.unwrap_or(3.75),
                     r: 3.75,
-                    stroke: "url(#colorwheel-cursor-border)",
+                    stroke: "url(#cursor-border)",
                     stroke_width: 2,
-                    fill: "hsl({hue}, 100%, 50%)"
+                    fill: fill
                 }
             }
         }
@@ -162,26 +269,41 @@ fn ColorWheelCursorSvg(hue: f64) -> Element {
 }
 
 fn ColorView() -> Element {
-    let color_state = use_context::<Color>();
-
-    let color = color_state;
+    let color_state = use_context::<Signal<ColorPickerState>>();
     rsx! {
         div {
             class: "color-view",
-            style: "--color-view-background: hsl({color.hue}deg, {color.saturation}%, {color.lightness}%);"
+            style: "--color-view-background: {color_state.read().get_rgb_string()};"
         }
     }
 }
 
-#[derive(Clone)]
-struct Color {
+struct ColorPickerState {
     hue: f64,
     saturation: f64,
-    lightness: f64,
+    brightness: f64,
+    alpha: f64,
 }
 
-fn cursor_position_to_hue(cursor_coordinates: Point2D<f64, PageSpace>, center_coordinates: Point2D<f64, PageSpace>) -> f64 {
-    log::trace!("cursor_coordinates: {:?}, center_coordinates: {:?}", cursor_coordinates, center_coordinates);
+impl ColorPickerState {
+    fn get_color(&self) -> Color {
+        Color::new_hsva(self.hue, self.saturation, self.brightness, self.alpha)
+    }
+
+    fn get_rgb_string(&self) -> String {
+        self.get_color().to_rgb_string()
+    }
+}
+
+fn cursor_position_to_hue(
+    cursor_coordinates: Point2D<f64, PageSpace>,
+    center_coordinates: Point2D<f64, PageSpace>,
+) -> f64 {
+    log::trace!(
+        "cursor_coordinates: {:?}, center_coordinates: {:?}",
+        cursor_coordinates,
+        center_coordinates
+    );
     let vector = cursor_coordinates - center_coordinates;
     let angle = vector.yx().angle_from_x_axis().positive().to_degrees() - 90f64;
     let angle = angle % 360f64;
@@ -190,5 +312,21 @@ fn cursor_position_to_hue(cursor_coordinates: Point2D<f64, PageSpace>, center_co
 }
 
 fn hue_to_css_rotation(hue: f64) -> f64 {
-    (450f64-hue).abs() % 360f64
+    (450f64 - hue).abs() % 360f64
+}
+
+fn saturation_to_x_axis(saturation: f64) -> f64 {
+    saturation * 100f64
+}
+
+fn brightness_to_y_axis(brightness: f64) -> f64 {
+    100f64 - (brightness * 100f64)
+}
+
+fn x_axis_to_saturation(x: f64) -> f64 {
+    x / 100f64
+}
+
+fn y_axis_to_brightness(y: f64) -> f64 {
+    (100f64 - y) / 100f64
 }
