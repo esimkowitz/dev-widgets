b00b7fa1be49329d2eef098c244b0f0a196c8044
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2025-01-27 15:27:21 -0800

    Fix Gitignore, remove unneeded assets

--- src/assets.rs
+++ src/assets.rs
@@ -1,6 +1,6 @@
 use manganis::*;
 
-pub const CSS: Asset = asset!("/assets/style/style.css");
-pub const BOOTSTRAP: Asset = asset!("/assets/js/bootstrap.min.js");
-pub const DARKMODE: Asset = asset!("/assets/js/darkmode.js");
-pub const GHPAGES: Asset = asset!("/assets/js/ghpages_redirect.js");
+pub const CSS: Asset = asset!("/public/style/style.css");
+pub const BOOTSTRAP: Asset = asset!("/public/js/bootstrap.min.js");
+pub const DARKMODE: Asset = asset!("/public/js/darkmode.js");
+pub const GHPAGES: Asset = asset!("/public/js/ghpages_redirect.js");
