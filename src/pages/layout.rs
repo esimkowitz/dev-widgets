726089c51fd851e95e444c84eae828aad9992616
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2026-01-03 22:03:51 -0500

    Add updated CI and precommits

--- src/pages/layout.rs
+++ src/pages/layout.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
-use dioxus_free_icons::Icon;
 use dioxus_free_icons::icons::fa_solid_icons::{FaChevronLeft, FaChevronRight};
+use dioxus_free_icons::Icon;
 use dioxus_sdk::storage::use_persistent;
 use serde::{Deserialize, Serialize};
 use strum::IntoEnumIterator;
