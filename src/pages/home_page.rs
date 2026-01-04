83b926c5db0277169fd027d96b4082e689fe4129
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2026-01-03 19:38:43 -0500

    fix cards

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -23,11 +23,11 @@ pub fn WidgetGrid(category_filter: Option<&'static str>) -> Element {
                 if category_filter.is_none() || route.get_widget_type_string() == category_filter {
                     for widget_route in route.get_widgets() {
                         if let Some(widget_entry) = widget_route.clone().get_widget_entry() {
-                            div { class: "card relative",
-                                div { class: "card-img-top", {(widget_entry.icon)()} }
+                            div { class: "card card-widget bg-base-100",
+                                figure { class: "card-figure", {(widget_entry.icon)()} }
                                 div { class: "card-body",
-                                    div { class: "card-title", {widget_entry.title} }
-                                    div { class: "card-text", {widget_entry.description} }
+                                    h2 { class: "card-title", {widget_entry.title} }
+                                    p { {widget_entry.description} }
                                     Link {
                                         class: "absolute inset-0",
                                         to: widget_route,
